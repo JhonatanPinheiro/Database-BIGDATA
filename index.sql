024d0c26d6796c3a3b984572f9bb3d938373337a
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2022-09-30 17:26:43 -0300

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,189 @@
+
+----------------------------------------------------SEÇÃO 33 - Introdução ao BIG DATA
+
+/*
+
+    Tal como Business Intelligence, Data Science é muito amplo, e costuma causar confusao
+quanto aos conceitos e áreas, a introdução se faz necessária para que o aluno saiba para 
+que área seguir
+
+Introdução
+
+- O que há em comum nas seguintes empresas?  Youtube,Instagram,Facebook,NetFlix,Drobox,Amazon ,GoogleDrive , entre outras....
+Resposta: Ferramenta Web e quantidados de dados em massa.
+
+dado -> é algo puro
+Informação -> conjunto de dados para uma finalidade
+
+
+
+GOOGLE TRENDINGS
+Criado em 2006, o Google Trends (trends significa tendências, em inglês) é uma ferramenta gratuita disponibilizada pelo Google que permite acompanhar a evolução do número de buscas por uma determinada palavra-chave ou tópico ao longo do tempo.
+Acessível em qualquer dispositivo, a plataforma também exibe gráficos para facilitar o entendimento sobre volumes de buscas, mostrando ainda as tendências em pesquisas no YouTube, por imagens e notícias.
+
+
+Onde podemos aplicar o DataScience: Varejo, Reconhecimento de Voz, Saúde, Comunicao e  Seguranca
+
+Estima-se que em torno de 90% dos dados armazenados  na web tenham sido gerados somente nos últimos 2 anos 
+
+
+O que é INSIGHT?
+RESPOSTA: Negócios apresentam problemas, que necessitam de solucoes, que por sua vez, necessitam de decisoes tomadas em cima de dados .
+ 1- clareza súbita na mente, no intelecto de um individuo, iluminação, estalo, luz.
+ 2- compreensão ou solução de um problema pela súbita captção mental dos elementos e relações adequados 
+
+*/
+
+
+------------------------------------------------------208. Introdução - Parte 02------------------------------------------
+
+
+-- Processo de decisao deve ser SEMPRE baseado em dados, por isso por  a importância dos insights
+
+-------------BUSINESS INTELLIGENCE x DATA SCIENCE-------------------------
+-- O que faz a ciência de dados? 
+----Resposta: Coleta dados de várias fontes, analisando e ajudando na tomada de decisoes de forma PREDITIVA a em grandes quantidades, gerando insights.  Planejamento não pe certexa. Nunca
+-- A ciência de dados é o processo que: [Extrai dados - De diversar fontes - Em diferentes velocidades - Processando grandes quantidades ] = Gerando Valor
+-- Data Science não é uma ferramenta. É um conjunto de métodos - > O BigData evidenciou o Ciência de dados
+-- Matemática , Estatística , Negócio e Mining e Visualização de Dados
+-- 
+
+
+------------------------------------------------209. Introdução - Parte 03---------------------------------------------------------
+------Area correlatas
+--Por ser um conjunto de métodos, a ciência de Dados possui algumas áreas correlatas, divididas em sub áreas
+----Computação
+--Machine Learning
+--Banco de dados 
+--Progração R e Python
+-------Negócios
+--Saúde
+--Vendas 
+--Industria
+----Estatística e Matemática
+--Machine Learning
+
+
+
+
+
+/*
+Estatistica e Matemática são a base da ciência de dados , e o 
+diferencial de métodos anterios 
+
+---------------Dois Pilares-------------
+ENGENHEIRO DE DADOS : Onde nos referimos ao profissional capaz de projetar o armazenamento de grandes quantidades de dados
+PENSAMENTO LÓGICO: Capacidade do Profissional de analisar os dados e aprender algo novo com ele, agregando aos negócios
+
+VIRTUALIZAÇÃO E CLOUD 
+
+O Armazenamento de grandes massas de dados consequentemente que um poder maior de  processamento. Isso favorece ao crescimento de áreas como BigData e NoSQL
+e com isso, o processamento horizontal. -> BIG DATA
+
+PROCESSAMENTO HORIZONTAL -> Eu pego toda a massa de dados e processo como um cloudster, eu divido o processo em outros servidores !
+
+
+
+Machine Learning?
+O conceito mais básico de Machine Learning     -----> Robôs atuando na linha de produção automotiva
+
+Machine Learning é uma sub área de inteligência Artificial 
+
+FUNCIONAMENTO GERAL DE MACHINE LEARNING
+Programamos as máquinas para aprendera, com os dados coletados.
+
+Coleta de dados 1> Aprendizagem com algoritmos de Machine Learning 2 -> Saída produzida para o Usuário 3
+
+DATA SCIENCE E ESTATÍSTICA
+Anteriormente vimos que o aprendizado de máquina dá-se pela coleta de dados, aprendizagem e saída de um resultado. De uma maneira simples, podemos definir estátistica da seguinte forma:
+Definição de um problema - Coleta dos Dados - Organizaçao dos Dados - Aplicação de métodos estatístico - saida
+
+
+
+ 
+*/
+
+
+-----------------------------------------------210. Introdução - Parte 04-----------------------------------------------
+
+/*
+DATA SCIENCE E ESTATÍSTICA
+Anteriormente vimos que o aprendizado de máquina dá-se pela coleta de dados, aprendizagem e saída de um resultado. De uma maneira simples, podemos definir estátistica da seguinte forma:
+Definição de um problema - Coleta dos Dados - Organizaçao dos Dados - Aplicação de métodos estatístico - saida
+
+
+VISÃO MARCRO DE PROJETOS DE DATA SCIENCE 
+
+PREPARAÇÃO -> Definição do Objetivo - Entender o problema - Conhecer os Dados
+
+ENGENHARIA DE DADOS -> Obtenção e limpeza dos dados  - Tranformação e enriquecimento - Persistência dos dados
+
+ANALYSTICS -> Ánalise Exploratória e Inferências  - Modelagem e Predição - Comunição
+
+PRODUÇÃO -> Produtos de Dados - Operancinalizar - Otimização
+
+
+O que eu vou entregar: Relatórios, StoryTelling (Saber contar a história do dado), Dashboards , Aplicações entre outros... 
+
+*/
+
+
+--------------------------------------------------------------211. Carreiras de DataScience-------------------------------------------
+-- Asssim como em Business Intelligence , temos 3 perfils macros dos profissionais de Data Science 
+
+/*
+O Cientista de Dados 
+
+Esse profissional tem um forte background em exatas,
+Como, ciência da computação, Mátematica e estátistica.
+É capaz de analisar grandes
+Quantidades de dados e chegar à conclusões .É um 
+perfil mais completo, equivalendo ao  Analista de Business Intelligence
+
+
+Analista de Négocios 
+Tal como na área de BI, a aréa de Data Science também precisa do 
+profissional com o perfil de negócios, que seja capaz de entender 
+o coração da empresa, sugerindo novas práticas ou negócios de forma
+a gerar mais valor 
+
+
+O Analista de Tecnologia
+Tal como na área de BI, a área de Data Science também precisa do profissional
+com perfil de tecnológico e de infraestrutura, pois devido à quantidade de dados,
+é necessario a administração de clusters para processamento paralelo
+dos dados , sejam eles relacionais não estruturados.
+
+O que é clounster?  São varias máquinas que irão processar a mesma massa de dados  
+ 
+
+
+
+*/
+
+----------------------------------------------------212. Big Data Real Time e Cloud Computing--------------------
+----------------------------------------------------213. O que é Big Data - Parte 01-----------------------------
+-- O que é BIG DATA?
+--É o Termo que descreve o imenso volume de dados - estruturados e não estruturados e não estruturados - que impactam os negócios no dia a dia . MAas o importante não é quantidade de dados. E sim que os dados que realmente importam
+
+
+-----------------------------------------------------214. O que é Big Data - Parte 02-----------------------------
+-----------------------------------------------------215. O que é Big Data - Parte 03-----------------------------
+
+/*
+IoT (Internet of Things)
+
+A internet das coisas é uma revolução tecnológica a fim de conectar dispositivos eletrônicos utlizados no dia-a-dia (como aparelhos eletrodomésticos,eletroportáteis,máquinas insdustriais , meios de transporte etc... )
+à internet , cujo desenvolvimento depende da inovação técnica dinâmica.
+
+
+HADOOP - É um sistema de Armazenamento e Processamento de dados em alto volume. Estão presentes em clusters baseados em Linux e composto por outros softwares Open Sources. Armazenam Dados, sejam eles Estruturados, semi estruturados ou não estruturados.
+
+O Hadoop é uma coleção de subprojetos relacionados para computação distribuída, sendo todos eles hospedados pela Apache Software Foundation 
+
+*/ 
+
+--------------------------------------------------------216. O que é Big Data - Parte 04-----------------------------
+/*
+O Hadoop é uma coleção de subprojetos relacionados para computação distribuída, sendo todos eles hospedados pela Apache Software Foundation 
+*/
